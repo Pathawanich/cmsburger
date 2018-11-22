749df03a49c26ae008194bf2339f8a12acc01c8c
Andreas Græsmo <ograsmo@gmail.com>
2018-11-22 20:36:18 +0100

    ChildChanges

--- wp-content/themes/shop-isle-pro-child/functions.php
+++ wp-content/themes/shop-isle-pro-child/functions.php
@@ -1,15 +1,9 @@
 <?php
-add_filter('woocommerce_product_add_to_cart_text', 'woo_custom_cart_button_text');
-  
-function woo_custom_cart_button_text() {
-  return __('Legg til', 'woocommerce');
-}
+add_filter('woocommerce_product_add_to_cart_text', 'translate_add_to_cart_button_text');
 
+add_filter('gettext', 'translate_view_cart_btn');
 
-
-add_filter('gettext', 'change_view_cart_btn');
-
-add_filter('gettext', 'change_checkout_btn');
+add_filter('gettext', 'translate_checkout_btn');
 
 add_filter( 'gettext', 'translate_product', 20, 3 );
 
@@ -19,16 +13,18 @@ add_filter('gettext', 'translate_quick_view');
 
 add_filter('gettext', 'translate_quant');
 
-add_filter( 'gettext', 'change_update_cart_text', 20, 3 );
+add_filter( 'gettext', 'translate_update_cart_text', 20, 3 );
 
-add_filter( 'gettext', 'change_cart_totals_text', 20, 3 );
-
-add_filter( 'gettext', 'change_separator_text', 20, 3 );
+add_filter( 'gettext', 'translate_cart_totals_text', 20, 3 );
 
 add_filter( 'gettext', 'translate_no_product_Text');
 
 remove_action( 'woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10 );
 remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5 );
+
+function translate_add_to_cart_button_text() {
+  return __('Legg til', 'woocommerce');
+}
 
 
 function translate_product( $translated, $text, $domain ) {
@@ -58,39 +54,32 @@ $translated = str_ireplace('Quantity', 'Antall', $translated);
 return $translated;
 }
 
-function change_update_cart_text( $translated, $text, $domain ) {
+function translate_update_cart_text( $translated, $text, $domain ) {
     if( is_cart() && $translated == 'Update cart' ){
         $translated = __('Oppdater kurven', 'woocommerce');
     }
     return $translated;
 }
 
-function change_cart_totals_text( $translated, $text, $domain ) {
+function translate_cart_totals_text( $translated, $text, $domain ) {
     if( is_cart() && $translated == 'Cart totals' ){
         $translated = __('Totalsum', 'woocommerce');
     }
     return $translated;
 }
 
-function change_separator_text( $translated, $text, $domain ) {
-    if( is_cart() && $translated == 'or' ){
-        $translated = __('eller', 'woocommerce');
-    }
-    return $translated;
-}
 
-//function
-function change_checkout_btn($checkout_btn){
+function translate_checkout_btn($checkout_btn){
   $checkout_btn= str_ireplace('Checkout', 'Til kassen', $checkout_btn);
   return $checkout_btn;
 }
 
-function change_view_cart_btn($view_cart_btn){
+function translate_view_cart_btn($view_cart_btn){
   $view_cart_btn= str_ireplace('View cart', 'Vis handlekurv', $view_cart_btn);
   return $view_cart_btn;
 }
 
-function woocommerce_button_proceed_to_checkout() {
+function translate_woocommerce_button_proceed_to_checkout() {
  $checkout_url = wc_get_checkout_url();
  ?>
  <a href="<?php echo $checkout_url; ?>" class="checkout-button button alt wc-forward"><?php _e( 'Gå til kassen', 'woocommerce' ); ?></a>
